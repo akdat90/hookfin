56b329d06315a2a842e9d7e09c874eef2b8af478
akdat90 <134646199+akdat90@users.noreply.github.com>
2023-06-14 11:39:07 +0000

    Updated buttons

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask,render_template
 
 app = Flask(__name__,template_folder='template')
 
+PRODUCTS = [
+  {
+    'id': 1,
+    'Product Name': 'Long term Investing',
+    'price': '20000'
+  },
+  {
+    'id': 2,
+    'Product Name': 'Short term Investing',
+    'price': '15000'
+  },
+  {
+    'id': 3,
+    'Product Name': 'Ultra term Investing',
+    'price': '10000'
+  }
+]
+
 
 @app.route("/")
 
 def fin_services():
-  return render_template("home.html")
+  return render_template("home.html", company_name="Hookup Finserve", products = PRODUCTS)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
