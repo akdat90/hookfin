65103badd2227895db340b6e670360e7bdf812b9
akdat90 <134646199+akdat90@users.noreply.github.com>
2023-06-15 03:42:32 +0000

    added footer and header

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_engine ="mysql+pymysql://8hftdvowswlulou064sv:pscale_pw_GoNoBnhRdxCaq5Vnr7JwNGRg89lhepOVIV5cBywihY9@aws.connect.psdb.cloud/hookup"
 
-engine = create_engine(db_connection_engine,
-                       connect_args={"ssl": {
-                         "ca": "/etc/ssl/cert.pem"
-                       }})
+my_secret = os.environ['link_to_db']
+engine = create_engine(
+  my_secret, 
+  connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  })
 
 with engine.connect() as conn:
   result = conn.execute(text("select * from products"))
